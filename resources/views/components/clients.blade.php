905253e32931761c96ac84f92105f2337dee4a54
OsamaAlmamri <yemencoder@gmail.com>
2022-11-27 22:15:52 +0300

    Update clients.blade.php

--- resources/views/components/clients.blade.php
+++ resources/views/components/clients.blade.php
@@ -1,20 +1,39 @@
-<section class="wrapper bg-light wrapper-border">
-  <div class="container py-14 py-md-16">
-    <h2 class="fs-15 text-uppercase text-muted text-center mb-8">قائمة لعملائنا</h2>
-    <div class="swiper-container clients mb-0" data-margin="30" data-dots="false" data-autoplay-timeout="3000" data-items-xxl="7" data-items-xl="6" data-items-lg="5" data-items-md="4" data-items-xs="2">
-      <div class="swiper">
-        <div class="swiper-wrapper">
-            @foreach($clients as $client)
-          <div class="swiper-slide px-5"><img src="{{$client->image()}}" alt="" /></div>
-            @endforeach
+<section class="wrapper bg-light">
+    <style type="text/css">
+        .swiper-navigation {
+            direction: ltr;
+        }
+    </style>
+    <div class="overflow-hidden">
+        <div class="container py-14 py-md-16">
+            <div class="row">
+                <div class="col-xl-7 col-xxl-6 mx-auto text-center">
+                    <i class="fal fa-pen-alt font-5"></i>
+                    <h2 class="display-5 text-center mt-2 mb-10">قائمة لعملائنا</h2>
+                </div>
+                <!--/column -->
+            </div>
 
+            <div class="swiper-container nav-bottom nav-color mb-14"
+                 data-margin="30" data-dots="false" data-nav="true"
+                 data-autoplay-timeout="3000"
+                 data-items-xxl="6" data-items-xl="5" data-items-lg="4" data-items-md="3" data-items-xs="2">
+                <div class="swiper overflow-visible pb-2">
+
+                    <div class="swiper-wrapper">
+                        @foreach($clients as $client)
+                            <div class="swiper-slide px-5"><img src="{{$client->image()}}" alt=""/></div>
+                        @endforeach
+
+                    </div>
+                    {{--          <div class="swiper-button-next"></div>--}}
+                    {{--          <div class="swiper-button-prev"></div>--}}
+                    {{--          <div class="swiper-pagination"></div>--}}
+
+                    <!--/.swiper-wrapper -->
+                </div>
+                <!-- /.swiper -->
+            </div>
         </div>
-        <!--/.swiper-wrapper -->
-      </div>
-      <!-- /.swiper -->
     </div>
-    <!-- /.swiper-container -->
-  </div>
-  <!-- /.container -->
 </section>
-<!-- /section -->
